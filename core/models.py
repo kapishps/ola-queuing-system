a37d2f472c85211d0f915e1bc5469226e44d3eb4
kspkidd <kapish.psingh.civ12@itbhu.ac.in>
2018-03-24 16:50:17 +0530

    initial prototype

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class customer(models.Model):
+    pass
+
+
+class driver(models.Model):
+    driver_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+
+
+class pickup_req(models.Model):
+    req_id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    accepted_at = models.DateTimeField(null=True, blank=True)
+
+    customer = models.ForeignKey(to=customer,on_delete=models.DO_NOTHING,)
+    driver = models.ForeignKey(to=driver,on_delete=models.DO_NOTHING,)
+
+    STATUS_TYPES = (
+        ('W', 'Waiting'),
+        ('O', 'Ongoing'),
+        ('C', 'Complete')
+    )
+    status = models.CharField(
+        max_length=1,
+        default='W',
+        choices=STATUS_TYPES
+    )
+
+
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def driver(request):
+    pass
+
+def customer(request):
+    pass
+
+def dashboard(request):
+    pass
+
+def create_req(request):
+    pass
+
+def pick_req(request):
+    pass
